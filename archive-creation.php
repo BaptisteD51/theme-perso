85bdae584cc80872b64e058694fb23b37b2b914e
Baptiste Dufour <dufourbaptiste08@gmail.com>
2024-06-13 22:42:46 +0200

    Added an archive page for creations + CSS

---
+++ archive-creation.php
@@ -0,0 +1,23 @@
+<?php get_header(); ?>
+<?php
+//var_dump(get_locale());
+?>
+<main>
+    <?php if (have_posts()) : ?>
+        <h1> Mes créations de sites Web </h1>
+        <ul class="archive-creation-list">
+            <?php while (have_posts()) : the_post(); ?>
+                <li>
+                    <a href="<?php the_permalink() ?>">
+                        <?php the_post_thumbnail(); ?>
+                        <h2><?php the_title(); ?></h2>
+                    </a>
+                </li>
+            <?php endwhile; ?>
+        </ul>
+    <?php else : ?>
+        <p>Pas de créations à afficher</p>
+    <?php endif; ?>
+</main>
+
+<?php get_footer(); ?>
