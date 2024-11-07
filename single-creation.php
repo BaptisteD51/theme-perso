ca79574a193216411b5469a900546bd51bf3dada
BaptisteD51 <dufourbaptiste08@gmail.com>
2024-11-07 22:16:54 +0100

    Css changes for technologies in single-creation.php

--- single-creation.php
+++ single-creation.php
@@ -4,7 +4,7 @@ if (have_posts()): ?>
     <main>
         <?php while (have_posts()): the_post(); ?>
             <h1><?php the_title(); ?></h1>
-            <div>
+            <div class="my-10">
                 Technologies :
                 <?php if (!empty(wp_get_post_terms(get_the_ID(), "technology"))) : foreach (wp_get_post_terms(get_the_ID(), "technology") as $technology) : ?>
                         <p class="creation-technology"><?php echo $technology->name; ?></p>
