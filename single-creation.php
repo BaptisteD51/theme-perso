de3dc335df3868177da7ba7e0d5e10b6456bda7b
BaptisteD51 <dufourbaptiste08@gmail.com>
2024-11-07 20:06:42 +0100

    display technologies on each creation single page

--- single-creation.php
+++ single-creation.php
@@ -1,17 +1,24 @@
 <?php get_header();
 
-if(have_posts()):?>
-<main>
-    <?php while(have_posts()): the_post();?>
-        <h1><?php the_title();?></h1>
-        <figure>
-            <?php the_post_thumbnail("large");?>
-        </figure>
-        <?php the_content();?>
-    <?php endwhile;?>
-</main>
-<?php else:?>
+if (have_posts()): ?>
+    <main>
+        <?php while (have_posts()): the_post(); ?>
+            <h1><?php the_title(); ?></h1>
+            <div>
+                Technologies :
+                <?php if (!empty(wp_get_post_terms(get_the_ID(), "technology"))) : foreach (wp_get_post_terms(get_the_ID(), "technology") as $technology) : ?>
+                        <p class="creation-technology"><?php echo $technology->name; ?></p>
+                <?php endforeach;
+                endif; ?>
+            </div>
+            <figure>
+                <?php the_post_thumbnail("large"); ?>
+            </figure>
+            <?php the_content(); ?>
+        <?php endwhile; ?>
+    </main>
+<?php else: ?>
     <p>Pas de création à afficher</p>
-<?php endif;?>
+<?php endif; ?>
 
-<?php get_footer();?>
+<?php get_footer(); ?>
